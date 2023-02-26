5bf34ba7f96a283fa0e49b37dac1b90d6952237e
MdMehedi-Hasan <mh19115@gmail.com>
2023-02-26 17:47:06 +0600

    watch section cmplete

--- src/Components/Center.js
+++ src/Components/Center.js
@@ -1,4 +1,6 @@
+import { Icon } from '@iconify/react';
 import React from 'react';
+import image from '../Assets/straight-watch.png'
 
 const Center = () => {
     return (
@@ -35,10 +37,50 @@ const Center = () => {
                     </div>
                 </div>
             </section>
-            <section>
-                <div></div>
-                <div></div>
-                <div></div>
+            <section className='container mx-auto'>
+                <div className='flex'>
+                    <div className='flex flex-col gap-10'>
+                        <div className='flex justify-start border-r-2 border-b-2 p-5 border-gray-500'><Icon icon="ion:watch" className='text-6xl' />
+                            <div>
+                                <h1 className='pt-0 mt-0'>Reliable</h1>
+                                <p className='font-sans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                            </div>
+                        </div>
+                        <div className='flex justify-start border-r-2 border-b-2 p-5 border-gray-500'><Icon icon="ion:watch" className='text-6xl' />
+                            <div>
+                                <h1 className='pt-0 mt-0'>Reliable</h1>
+                                <p className='font-sans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                            </div>
+                        </div>
+                        <div className='flex justify-start border-r-2 border-b-2 p-5 border-gray-500'><Icon icon="ion:watch" className='text-6xl' />
+                            <div>
+                                <h1 className='pt-0 mt-0'>Reliable</h1>
+                                <p className='font-sans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex justify-center items-center'><img src={image} alt="" /></div>
+                    <div className='flex flex-col gap-10'>
+                        <div className='flex justify-start border-r-2 border-b-2 p-5 border-gray-500'><Icon icon="ion:watch" className='text-6xl' />
+                            <div>
+                                <h1 className='pt-0 mt-0'>Reliable</h1>
+                                <p className='font-sans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                            </div>
+                        </div>
+                        <div className='flex justify-start border-r-2 border-b-2 p-5 border-gray-500'><Icon icon="ion:watch" className='text-6xl' />
+                            <div>
+                                <h1 className='pt-0 mt-0'>Reliable</h1>
+                                <p className='font-sans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                            </div>
+                        </div>
+                        <div className='flex justify-start border-r-2 border-b-2 p-5 border-gray-500'><Icon icon="ion:watch" className='text-6xl' />
+                            <div>
+                                <h1 className='pt-0 mt-0'>Reliable</h1>
+                                <p className='font-sans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
     );
