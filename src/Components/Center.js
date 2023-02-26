82a01be65d18807053780edac033ed00316f82ad
MdMehedi-Hasan <mh19115@gmail.com>
2023-02-26 17:23:40 +0600

    project initiated

---
+++ src/Components/Center.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const Center = () => {
+    return (
+        <div>
+            <section className='flex gap-10 justify-center'>
+                <div class="card w-auto bg-base-100 shadow-xl">
+                    <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card w-auto bg-base-100 shadow-xl">
+                    <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card w-auto bg-base-100 shadow-xl">
+                    <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div></div>
+                <div></div>
+                <div></div>
+            </section>
+        </div>
+    );
+};
+
+export default Center;
---
+++ src/Components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import watch from '../Assets/angled-watch.png'
+import blob from '../Assets/blob (1).svg'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div id='header' className='bg-white flex justify-center relative py-10 h-screen'>
+            <div className='container flex items-center justify-between'>
+                <div className='flex flex-col'>
+                    <div className='flex-1'></div>
+                    <h1 className='text-8xl text-black title flex-1'>Helio G92 <br/> Smart Watch</h1>
+                    <div className='flex-1'>
+                    <button type="" className='btn btn-success text-gray-600'>Order Now</button>
+                    </div>
+                </div>
+                <div className='relative'>
+                    <img src={watch} alt="" className='relative z-50 w-10/12'/>
+                    <img src={blob} alt="" className='absolute bottom-0 z-10 w-full'/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
