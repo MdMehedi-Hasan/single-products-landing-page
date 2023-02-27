d898b5543bfa86d83a2ade5ad05029354e0dbcd1
MdMehedi-Hasan <mh19115@gmail.com>
2023-02-27 14:36:13 +0600

    top three sections done

--- src/Components/Center.js
+++ src/Components/Center.js
@@ -7,10 +7,17 @@ import trophy from '../Assets/trophy.png'
 import map from '../Assets/map.jpg'
 import Review from './Review';
 import watches from '../Assets/multiple-watches2.png'
+
+import gps from '../Assets/location.png'
+import security from '../Assets/insurance.png'
+import idea from '../Assets/light-bulb.png'
+import bills from '../Assets/invoice.png'
+import tech from '../Assets/technology.png'
+import heart from '../Assets/love.png'
 const Center = () => {
     return (
         <div className='mt-[-40px] border'>
-            <section className='flex justify-center pb-40'>
+            <section className='flex justify-center pb-40 bg-base-200'>
                 <div className='container flex justify-center gap-5'>
                     <div class="card w-auto bg-white shadow-xl text-center py-5">
                         <figure><img src={connect} alt="Connect" className='w-24' /></figure>
@@ -26,7 +33,7 @@ const Center = () => {
                             <p>Lorem ipsum dolor sit amet, consectetur de elit, sed do tempor incididunt ut labore eta rehenderit in voluptate velit.</p>
                         </div>
                     </div>
-                    <div class="card w-auto bg-base-200 shadow-xl text-center py-5">
+                    <div class="card w-auto bg-white shadow-xl text-center py-5">
                         <figure><img src={trophy} alt="trophy" className='w-24' /></figure>
                         <div class="card-body">
                             <h2 class="text-2xl font-semibold">Yay! Done</h2>
@@ -39,40 +46,46 @@ const Center = () => {
                 <div className='container'>
                     <h1 className='text-center text-6xl font-semibold'>The Benefits</h1>
                     <p className='text-center text-2xl text-gray-500 w-6/12 mx-auto py-4'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-                    <div className='grid grid-cols-3 pt-20 pb-24'>
-                        <div className='flex justify-start '><Icon icon="ion:watch" className='text-6xl' />
+                    <div className='grid grid-cols-3 pt-20 pb-24 gap-10'>
+                        <div className='flex justify-start gap-3'>
+                            <img src={gps} alt="" className='w-12 h-12'/>
                             <div>
-                                <h1 className='pt-0 mt-0'>Reliable</h1>
+                                <h1 className='pt-0 mt-0 text-xl font-semibold'>GPS Tracking</h1>
                                 <p className='font-sans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
                             </div>
                         </div>
-                        <div className='flex justify-start '><Icon icon="ion:watch" className='text-6xl' />
+                        <div className='flex justify-start gap-3'>
+                        <img src={security} alt="" className='w-12 h-12'/>
                             <div>
-                                <h1 className='pt-0 mt-0'>Reliable</h1>
+                                <h1 className='pt-0 mt-0 text-xl font-semibold'>Security first</h1>
                                 <p className='font-sans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
                             </div>
                         </div>
-                        <div className='flex justify-start '><Icon icon="ion:watch" className='text-6xl' />
+                        <div className='flex justify-start gap-3'>
+                        <img src={idea} alt="" className='w-12 h-12'/>
                             <div>
-                                <h1 className='pt-0 mt-0'>Reliable</h1>
+                                <h1 className='pt-0 mt-0 text-xl font-semibold'>Innovative idea</h1>
                                 <p className='font-sans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
                             </div>
                         </div>
-                        <div className='flex justify-start '><Icon icon="ion:watch" className='text-6xl' />
+                        <div className='flex justify-start gap-3'>
+                        <img src={bills} alt="" className='w-12 h-12'/>
                             <div>
-                                <h1 className='pt-0 mt-0'>Reliable</h1>
+                                <h1 className='pt-0 mt-0 text-xl font-semibold'>Save your bills</h1>
                                 <p className='font-sans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
                             </div>
                         </div>
-                        <div className='flex justify-start '><Icon icon="ion:watch" className='text-6xl' />
+                        <div className='flex justify-start gap-3'>
+                        <img src={tech} alt="" className='w-12 h-12'/>
                             <div>
-                                <h1 className='pt-0 mt-0'>Reliable</h1>
+                                <h1 className='pt-0 mt-0 text-xl font-semibold'>Proven technology</h1>
                                 <p className='font-sans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
                             </div>
                         </div>
-                        <div className='flex justify-start '><Icon icon="ion:watch" className='text-6xl' />
+                        <div className='flex justify-start gap-3'>
+                        <img src={heart} alt="" className='w-12 h-12'/>
                             <div>
-                                <h1 className='pt-0 mt-0'>Reliable</h1>
+                                <h1 className='pt-0 mt-0 text-xl font-semibold'>Heartbeat Analysis</h1>
                                 <p className='font-sans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
                             </div>
                         </div>
@@ -80,7 +93,7 @@ const Center = () => {
                 </div>
             </section>
             <section className='flex justify-center'>
-                <div className='container grid grid-cols-2 items-center pb-40'>
+                <div className='container grid grid-cols-2 items-center py-40'>
                     <div>
                         <img src={watches} alt="watches" />
                     </div>
@@ -88,26 +101,26 @@ const Center = () => {
                         <h1 className='text-4xl font-semibold'>Product Description</h1>
                         <p className='py-5'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-                        <a href="#order"><button type="" className='btn btn-success mt-5'>Order Now</button></a>
+                        <a href="#order"><button type="" className='btn btn-success mt-5 text-white'>Order Now</button></a>
                     </div>
                 </div>
             </section>
             <section className='flex justify-center bg-base-200 py-40'>
                 <div className='flex container'>
                     <div className='flex flex-col gap-20'>
-                        <div className='flex justify-start '><Icon icon="ion:watch" className='text-6xl' />
+                        <div className='flex justify-start border-r-2 border-b-2 p-5 border-gray-200'><Icon icon="ion:watch" className='text-6xl' />
                             <div>
                                 <h1 className='pt-0 mt-0'>Reliable</h1>
                                 <p className='font-sans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
                             </div>
                         </div>
-                        <div className='flex justify-start '><Icon icon="ion:watch" className='text-6xl' />
+                        <div className='flex justify-start border-r-2 border-b-2 p-5 border-gray-200'><Icon icon="ion:watch" className='text-6xl' />
                             <div>
                                 <h1 className='pt-0 mt-0'>Reliable</h1>
                                 <p className='font-sans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
                             </div>
                         </div>
-                        <div className='flex justify-start '><Icon icon="ion:watch" className='text-6xl' />
+                        <div className='flex justify-start border-r-2 border-b-2 p-5 border-gray-200'><Icon icon="ion:watch" className='text-6xl' />
                             <div>
                                 <h1 className='pt-0 mt-0'>Reliable</h1>
                                 <p className='font-sans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
@@ -184,7 +197,7 @@ const Center = () => {
             <section>
                 <Review />
             </section>
-            <section id="order" className='flex justify-center py-40'>
+            <section id="order" className=' flex justify-center py-40'>
                 <div className='container flex justify-center gap-40'>
                     {/* <div class="card image-full">
                         <figure><img src={map} alt="map" /></figure>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import watch from '../Assets/angled-watch.png'
-import blob from '../Assets/blob (1).svg'
+import blob from '../Assets/blob-haikei.svg'
 import wave from '../Assets/wave.svg'
 import './Header.css'
 
@@ -10,13 +10,14 @@ const Header = () => {
             <div className='container flex items-center justify-between'>
                 <div className='flex flex-col'>
                     <div className='flex-1'></div>
-                    <h1 className='text-8xl text-black title flex-1 relative z-20'>Helio G92 <br/> Smart Watch</h1>
+                    <h1 className='text-8xl title flex-1'>Helio G92 <br/> Smart Watch</h1>
                     <div className='flex-1'>
-                    <a href="#order"><button type="" className='btn btn-success text-gray-600 relative z-20'>Order Now</button></a>
+                    <a href="#order"><button type="" className='btn bg-white text-black border-white hover:bg-white hover:text-black hover:border-white mt-10'>Order Now</button></a>
                     </div>
                 </div>
-                <div className=''>
-                    <img src={watch} alt="" className='relative z-20'/>
+                <div className='relative'>
+                    <img src={watch} alt="" className=''/> {/* relative z-10 */}
+                    {/* <img src={blob} alt="" className='absolute top-10 z-0'/> */}
                 </div>
             </div>
         </div>
