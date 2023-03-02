f9f092c696018743cc8f51a2814a201dfadc4f1e
MdMehedi-Hasan <mh19115@gmail.com>
2023-03-02 17:18:46 +0600

    animation added

--- src/Components/Center.js
+++ src/Components/Center.js
@@ -303,10 +303,10 @@ const Center = () => {
             </section>
             <section id='description' className='flex justify-center'>
                 <div className='container grid grid-cols-2 items-center py-40'>
-                    <div>
+                    <div data-aos="fade-right" data-aos-delay="500" data-aos-duration="1000" data-aos-easing="ease-in-sine">
                         <img src={watches} alt="watches" />
                     </div>
-                    <div>
+                    <div data-aos="fade-left" data-aos-delay="500" data-aos-duration="1000" data-aos-easing="ease-in-sine">
                         <h1 className='text-4xl font-semibold'>Product Description</h1>
                         <p className='py-5'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
@@ -317,21 +317,21 @@ const Center = () => {
             <section className='flex justify-center bg-base-200 py-40'>
                 <div className='grid grid-cols-3 items-center container'>
                     <div className='flex flex-col gap-10'>
-                        <div className='flex gap-3 justify-start border-r-2 border-b-2 p-5 border-gray-200'>
+                        <div className='flex gap-3 justify-start border-r-2 border-b-2 p-5 border-gray-200' data-aos="zoom-in" data-aos-delay="300" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <img src={key} alt="" className='h-12' />
                             <div>
                                 <h1 className='pt-0 mt-0 text-xl font-bold'>Fast and secure</h1>
                                 <p className='font-sans text-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
                             </div>
                         </div>
-                        <div className='flex gap-3 justify-start border-r-2 border-b-2 p-5 border-gray-200'>
+                        <div className='flex gap-3 justify-start border-r-2 border-b-2 p-5 border-gray-200' data-aos="zoom-in" data-aos-delay="700" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <img src={voice} alt="" className='h-12' />
                             <div>
                                 <h1 className='pt-0 mt-0 text-xl font-bold'>Voice Assistant</h1>
                                 <p className='font-sans text-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
                             </div>
                         </div>
-                        <div className='flex gap-3 justify-start border-r-2 border-b-2 p-5 border-gray-200'>
+                        <div className='flex gap-3 justify-start border-r-2 border-b-2 p-5 border-gray-200' data-aos="zoom-in" data-aos-delay="1100" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <img src={apps} alt="" className='h-12' />
                             <div>
                                 <h1 className='pt-0 mt-0 text-xl font-bold'>Apps you love</h1>
@@ -339,23 +339,23 @@ const Center = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='flex justify-center items-center'><img src={image} alt="" /></div>
+                    <div className='flex justify-center items-center' data-aos="zoom-in" data-aos-delay="100" data-aos-duration="500" data-aos-easing="ease-in-sine"><img src={image} alt="" /></div>
                     <div className='flex flex-col gap-10'>
-                        <div className='flex gap-3 justify-start border-l-2 border-b-2 p-5 border-gray-200'>
+                        <div className='flex gap-3 justify-start border-l-2 border-b-2 p-5 border-gray-200' data-aos="zoom-in" data-aos-delay="500" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <img src={like} alt="" className='h-12' />
                             <div>
                                 <h1 className='pt-0 mt-0 text-xl font-bold'>Stay in touch</h1>
                                 <p className='font-sans text-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
                             </div>
                         </div>
-                        <div className='flex gap-3 justify-start border-l-2 border-b-2 p-5 border-gray-200'>
+                        <div className='flex gap-3 justify-start border-l-2 border-b-2 p-5 border-gray-200' data-aos="zoom-in" data-aos-delay="900" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <img src={person} alt="" className='h-12' />
                             <div>
                                 <h1 className='pt-0 mt-0 text-xl font-bold'>Designed for you</h1>
                                 <p className='font-sans text-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
                             </div>
                         </div>
-                        <div className='flex gap-3 justify-start border-l-2 border-b-2 p-5 border-gray-200'>
+                        <div className='flex gap-3 justify-start border-l-2 border-b-2 p-5 border-gray-200' data-aos="zoom-in" data-aos-delay="1300" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <img src={watch} alt="" className='h-12' />
                             <div>
                                 <h1 className='pt-0 mt-0 text-xl font-bold'>Precise timepiece</h1>
@@ -367,17 +367,17 @@ const Center = () => {
             </section>
             <section className='flex justify-center bg-white py-20'>
                 <div className='container'>
-                    <h1 className='text-center text-6xl font-semibold'>TECH SPECS</h1>
-                    <p className='text-center text-2xl text-gray-500 w-6/12 mx-auto py-4'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                    <h1 className='text-center text-6xl font-semibold' data-aos="zoom-in" data-aos-delay="100" data-aos-duration="500" data-aos-easing="ease-in-sine">TECH SPECS</h1>
+                    <p className='text-center text-2xl text-gray-500 w-6/12 mx-auto py-4' data-aos="fade-up" data-aos-delay="600" data-aos-duration="1200" data-aos-easing="ease-in-sine">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                     <div className='grid grid-cols-3 pt-20 pb-24 gap-x-20 gap-y-10'>
-                        <div className='border-b mx-16'>
+                        <div className='border-b mx-16' data-aos="fade-right" data-aos-delay="1400" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <h1 className='text-2xl font-semibold'>Compatibility</h1>
                             <p className='font-sans text-gray-500'>iPhone iOS 7 & up
                                 Android 4.3 & uptexts
                                 Bluetooth 4.0
                                 Notification for calls / texts</p>
                         </div>
-                        <div className='border-b mx-16'>
+                        <div className='border-b mx-16' data-aos="fade-up-right" data-aos-delay="1500" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <h1 className='text-2xl font-semibold'>App</h1>
                             <p className='font-sans text-gray-500'>Set daily goals
                                 Analyze history
@@ -386,7 +386,7 @@ const Center = () => {
                                 Connect to Google fit
                                 Watch firmware upgrade</p>
                         </div>
-                        <div className='border-b mx-16'>
+                        <div className='border-b mx-16' data-aos="fade-up-right" data-aos-delay="1600" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <h1 className='text-2xl font-semibold'>Vibration silent alarms</h1>
                             <p className='font-sans text-gray-500'>Phone notifications
                                 Daily goal reached
@@ -394,7 +394,7 @@ const Center = () => {
                                 Location assistance
                                 Accessiblity features</p>
                         </div>
-                        <div className='border-b mx-16'>
+                        <div className='border-b mx-16' data-aos="fade-up-right" data-aos-delay="1700" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <h1 className='text-2xl font-semibold'>Watch</h1>
                             <p className='font-sans text-gray-500'>12.6mm thickness
                                 3.16L stainless steel case
@@ -402,31 +402,31 @@ const Center = () => {
                                 5 ATM water resistant
                                 Sapphire glass crystal</p>
                         </div>
-                        <div className='border-b mx-16'>
+                        <div className='border-b mx-16' data-aos="fade-up-right" data-aos-delay="1800" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <h1 className='text-2xl font-semibold'>Straps</h1>
                             <p className='font-sans text-gray-500'>Italian leather with natural lining
                                 Comfort sport fabric
                                 Quick-release pins</p>
                         </div>
-                        <div className='border-b mx-16'>
+                        <div className='border-b mx-16' data-aos="fade-up-right" data-aos-delay="1900" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <h1 className='text-2xl font-semibold'>Activity tracking</h1>
                             <p className='font-sans text-gray-500'>Step counting
                                 Distance
                                 Calories burned
                                 Swimming (strokes)</p>
                         </div>
-                        <div className='border-b mx-16'>
+                        <div className='border-b mx-16' data-aos="fade-up-right" data-aos-delay="2000" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <h1 className='text-2xl font-semibold'>Timekeeping</h1>
                             <p className='font-sans text-gray-500'>Swiss ETA movement
                                 Accurate, reliable</p>
                         </div>
-                        <div className='border-b mx-16'>
+                        <div className='border-b mx-16' data-aos="fade-up-right" data-aos-delay="2100" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <h1 className='text-2xl font-semibold'>Battery power</h1>
                             <p className='font-sans text-gray-500'>Standard CR2025 6+ months
                                 Standard 364 for timekeeping 5+ years
                                 Solar Power available as aupgrade</p>
                         </div>
-                        <div className='border-b mx-16'>
+                        <div className='border-b mx-16' data-aos="fade-up-right" data-aos-delay="2200" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <h1 className='text-2xl font-semibold'>Sensors</h1>
                             <p className='font-sans text-gray-500'>MEMS 3-axis accelerometer
                                 Highly accurate
--- src/Components/Faq.js
+++ src/Components/Faq.js
@@ -1,20 +1,18 @@
 import { Icon } from '@iconify/react';
 import React from 'react';
-// import image from '../Assets/faq.png'
 import image from '../Assets/faq-blob1.png'
-import blob from '../Assets/faq-blob1.svg'
 
 const Faq = () => {
     return (
         <div id='faq' className='flex justify-center'>
             <div className='pt-20'>
                 <div className=''>
-                    <h1 className='text-6xl text-center font-semibold'>FAQ</h1>
-                    <p className='mx-auto mt-3 text-2xl font-semibold text-gray-400 max-w-2xl text-center'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                    <h1 className='text-6xl text-center font-semibold' data-aos="zoom-in" data-aos-delay="100" data-aos-duration="500" data-aos-easing="ease-in-sine">FAQ</h1>
+                    <p className='mx-auto mt-3 text-2xl font-semibold text-gray-400 max-w-2xl text-center' data-aos="fade-up" data-aos-delay="600" data-aos-duration="1200" data-aos-easing="ease-in-sine">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                 </div>
                 <div className='container grid grid-cols-2 items-start justify-between my-40'>
                     <div className='flex flex-col gap-3'>
-                        <div tabIndex='0' className="collapse collapse-plus collapse-open">
+                        <div tabIndex='0' className="collapse collapse-plus" data-aos="fade-right" data-aos-delay="100" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <input type="checkbox" className="peer" />
                             <div className="collapse-title flex items-center gap-2 bg-green-400 rounded text-white font-bold text-xl">
                                 <Icon icon="mdi:question-mark-circle" className='text-2xl' />
@@ -24,7 +22,7 @@ const Faq = () => {
                                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
                             </div>
                         </div>
-                        <div className="collapse collapse-plus">
+                        <div className="collapse collapse-plus" data-aos="fade-right" data-aos-delay="100" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <input type="checkbox" className="peer" />
                             <div className="collapse-title flex items-center gap-2 bg-green-400 rounded text-white font-bold text-xl">
                                 <Icon icon="mdi:question-mark-circle" className='text-2xl' />
@@ -34,7 +32,7 @@ const Faq = () => {
                                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
                             </div>
                         </div>
-                        <div className="collapse collapse-plus">
+                        <div className="collapse collapse-plus" data-aos="fade-right" data-aos-delay="100" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <input type="checkbox" className="peer" />
                             <div className="collapse-title flex items-center gap-2 bg-green-400 rounded text-white font-bold text-xl">
                                 <Icon icon="mdi:question-mark-circle" className='text-2xl' />
@@ -44,7 +42,7 @@ const Faq = () => {
                                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
                             </div>
                         </div>
-                        <div className="collapse collapse-plus">
+                        <div className="collapse collapse-plus" data-aos="fade-right" data-aos-delay="100" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <input type="checkbox" className="peer" />
                             <div className="collapse-title flex items-center gap-2 bg-green-400 rounded text-white font-bold text-xl">
                                 <Icon icon="mdi:question-mark-circle" className='text-2xl' />
@@ -54,7 +52,7 @@ const Faq = () => {
                                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
                             </div>
                         </div>
-                        <div className="collapse collapse-plus">
+                        <div className="collapse collapse-plus" data-aos="fade-right" data-aos-delay="100" data-aos-duration="500" data-aos-easing="ease-in-sine">
                             <input type="checkbox" className="peer" />
                             <div className="collapse-title flex items-center gap-2 bg-green-400 rounded text-white font-bold text-xl">
                                 <Icon icon="mdi:question-mark-circle" className='text-2xl' />
@@ -65,10 +63,9 @@ const Faq = () => {
                             </div>
                         </div>
                     </div>
-                    <div>
+                    <div data-aos="fade-left" data-aos-delay="100" data-aos-duration="500" data-aos-easing="ease-in-sine">
                         <figure className='flex justify-end relative'>
                             <img src={image} alt="Trulli" className='relative z-10'/>
-                            {/* <img src={blob} alt="" className='absolute z-0 right-0 top-0'/> */}
                         </figure>
                     </div>
                 </div>
