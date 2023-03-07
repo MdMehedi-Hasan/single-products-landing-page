a8be3981a5de9f9bad6899508dfe2cdf95ae92da
MdMehedi-Hasan <mh19115@gmail.com>
2023-03-07 15:08:27 +0600

    review content updated

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -30,8 +30,8 @@ const Review = () => {
                                 <img src={review1} alt='client' />
                             </div>
                         </div>
-                        <h1 className='pb-5 text-xl font-semibold text-gray-500'>Mrs. Angelina Jolly</h1>
-                        <p className='pb-10'><Icon icon="bxs:quote-alt-left" className='inline-block -mt-3 mr-1 text-gray-500'/>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.<Icon icon="bxs:quote-alt-right" className='inline-block -mb-1 ml-1 text-gray-500'/></p>
+                        <h1 className='pb-5 text-xl font-semibold text-gray-500'>Andrew Duke</h1>
+                        <p className='pb-10'><Icon icon="bxs:quote-alt-left" className='inline-block -mt-3 mr-1 text-gray-500'/>I love the watch. It is extreamly beautiful and matches my personality. As a content creator I have to maintain time and track daily activitis. This watch helps me to do my works on time and become myself more productive.<Icon icon="bxs:quote-alt-right" className='inline-block -mb-1 ml-1 text-gray-500'/></p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className='pb-10 pt-2'>
@@ -41,8 +41,8 @@ const Review = () => {
                                 <img src={review2} alt='client' />
                             </div>
                         </div>
-                        <h1 className='pb-5 text-xl font-semibold text-gray-500'>Mrs. Angelina Jolly</h1>
-                        <p className='pb-10'><Icon icon="bxs:quote-alt-left" className='inline-block -mt-3 mr-1 text-gray-500'/>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.<Icon icon="bxs:quote-alt-right" className='inline-block -mb-1 ml-1 text-gray-500'/></p>
+                        <h1 className='pb-5 text-xl font-semibold text-gray-500'>Helena Jolly</h1>
+                        <p className='pb-10'><Icon icon="bxs:quote-alt-left" className='inline-block -mt-3 mr-1 text-gray-500'/>Got the product as expected. I ordered the product at morning and got it by evening. They have very strong cutomer service. Would love to buy from them again.<Icon icon="bxs:quote-alt-right" className='inline-block -mb-1 ml-1 text-gray-500'/></p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className='pb-10 pt-2'>
@@ -52,8 +52,8 @@ const Review = () => {
                                 <img src={review3} alt='client' />
                             </div>
                         </div>
-                        <h1 className='pb-5 text-xl font-semibold text-gray-500'>Mrs. Angelina Jolly</h1>
-                        <p className='pb-10'><Icon icon="bxs:quote-alt-left" className='inline-block -mt-3 mr-1 text-gray-500'/>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.<Icon icon="bxs:quote-alt-right" className='inline-block -mb-1 ml-1 text-gray-500'/></p>
+                        <h1 className='pb-5 text-xl font-semibold text-gray-500'>Ameli Jeorge</h1>
+                        <p className='pb-10'><Icon icon="bxs:quote-alt-left" className='inline-block -mt-3 mr-1 text-gray-500'/>I recently got my hands on a the watch and I have to say, I am thoroughly impressed! The watch is sleek and stylish, and it has all the features I could possibly need. The interface is easy to navigate and I love the fact that I can customize the watch face to my liking<Icon icon="bxs:quote-alt-right" className='inline-block -mb-1 ml-1 text-gray-500'/></p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className='pb-10 pt-2'>
@@ -63,8 +63,8 @@ const Review = () => {
                                 <img src={review4} alt='client' />
                             </div>
                         </div>
-                        <h1 className='pb-5 text-xl font-semibold text-gray-500'>Mrs. Angelina Jolly</h1>
-                        <p className='pb-10'><Icon icon="bxs:quote-alt-left" className='inline-block -mt-3 mr-1 text-gray-500'/>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.<Icon icon="bxs:quote-alt-right" className='inline-block -mb-1 ml-1 text-gray-500'/></p>
+                        <h1 className='pb-5 text-xl font-semibold text-gray-500'>Mr. Fragement</h1>
+                        <p className='pb-10'><Icon icon="bxs:quote-alt-left" className='inline-block -mt-3 mr-1 text-gray-500'/>The fitness tracking features is a huge plus for me, as I can easily track my daily steps, heart rate, and even my sleep patterns. The watch also seamlessly connects to my phone, allowing me to receive notifications and even take calls from my wrist.<Icon icon="bxs:quote-alt-right" className='inline-block -mb-1 ml-1 text-gray-500'/></p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className='pb-10 pt-2'>
@@ -75,7 +75,7 @@ const Review = () => {
                             </div>
                         </div>
                         <h1 className='pb-5 text-xl font-semibold text-gray-500'>Mrs. Angelina Jolly</h1>
-                        <p className='pb-10'><Icon icon="bxs:quote-alt-left" className='inline-block -mt-3 mr-1 text-gray-500'/>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.<Icon icon="bxs:quote-alt-right" className='inline-block -mb-1 ml-1 text-gray-500'/></p>
+                        <p className='pb-10'><Icon icon="bxs:quote-alt-left" className='inline-block -mt-3 mr-1 text-gray-500'/>I love that I can customize the watch face to my liking. The fitness tracking features are spot-on, accurately tracking my steps, heart rate, and even my sleep patterns.<Icon icon="bxs:quote-alt-right" className='inline-block -mb-1 ml-1 text-gray-500'/></p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className='pb-10 pt-2'>
@@ -85,8 +85,8 @@ const Review = () => {
                                 <img src={review3} alt='client' />
                             </div>
                         </div>
-                        <h1 className='pb-5 text-xl font-semibold text-gray-500'>Mrs. Angelina Jolly</h1>
-                        <p className='pb-10'><Icon icon="bxs:quote-alt-left" className='inline-block -mt-3 mr-1 text-gray-500'/>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.<Icon icon="bxs:quote-alt-right" className='inline-block -mb-1 ml-1 text-gray-500'/></p>
+                        <h1 className='pb-5 text-xl font-semibold text-gray-500'>Oliver</h1>
+                        <p className='pb-10'><Icon icon="bxs:quote-alt-left" className='inline-block -mt-3 mr-1 text-gray-500'/>The watch seamlessly connects to my phone, allowing me to receive notifications and even take calls directly from my wrist. Overall, I would highly recommend this smartwatch to anyone looking for a convenient and stylish way to stay on top of their busy lives while also staying healthy and active.<Icon icon="bxs:quote-alt-right" className='inline-block -mb-1 ml-1 text-gray-500'/></p>
                     </div>
                 </SwiperSlide>
             </Swiper>
